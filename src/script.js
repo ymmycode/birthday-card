a2e450c434617e9b20b16510379f5d2c2b29e0f6
Achmat Fauzi <achmatfauzi86@gmail.com>
2022-04-17 07:30:16 +0700

    changing time out controls

--- src/script.js
+++ src/script.js
@@ -20,6 +20,6 @@ button.addEventListener(`click`, ()=>
 
     tl.play()
 
-    setTimeout(() => {experience.enableControls()}, 30000)
+    setTimeout(() => {experience.enableControls()}, 40000)
 
 })
